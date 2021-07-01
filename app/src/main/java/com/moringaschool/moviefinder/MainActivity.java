f7d035e59a742bfcad4eb0561a12ac76783e8283
tonyguthiga <toniguthiga93@gmail.com>
2021-07-01 19:23:21 +0300

    add intent,add new activity

--- app/src/main/java/com/moringaschool/moviefinder/MainActivity.java
+++ app/src/main/java/com/moringaschool/moviefinder/MainActivity.java
@@ -2,23 +2,32 @@ package com.moringaschool.moviefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindMoviesButton;
+    private EditText mLocationEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindMoviesButton = (Button)findViewById(R.id.findMoviesButton);
         mFindMoviesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, MoviesActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/moringaschool/moviefinder/MoviesActivity.java
@@ -0,0 +1,21 @@
+package com.moringaschool.moviefinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MoviesActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the movies available: " + location);
+    }
+}
