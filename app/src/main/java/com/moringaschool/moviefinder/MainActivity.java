5414351421b127e65cc190f0ac9150e2972a58a9
tonyguthiga <toniguthiga93@gmail.com>
2021-07-01 18:23:27 +0300

    add toast

--- app/src/main/java/com/moringaschool/moviefinder/MainActivity.java
+++ app/src/main/java/com/moringaschool/moviefinder/MainActivity.java
@@ -3,12 +3,23 @@ package com.moringaschool.moviefinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mFindMoviesButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindMoviesButton = (Button)findViewById(R.id.findMoviesButton);
+        mFindMoviesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
